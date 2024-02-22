742041024c16603f1b611fe17f626f6b24a859a9
ANKUR SINGH <ankursinghtg2003@gmail.com>
2024-02-23 00:28:57 +0530

    added buttons and bg color

--- Q2.py
+++ Q2.py
@@ -14,41 +14,58 @@ titleLabel.pack()
 frame2=Frame(root)
 frame2.pack()
 
+
+turn ="X"
 def play(event):
+    global turn
     button=event.widget
 
-    button["text"]=X
+    if turn=="X":
+        button["text"]="X"
+        turn="O"
+    else:
+        button["text"]="O"
+        turn="X"
 
-#first row
-button1=Button(frame2, text=" ",width=16,height=8)
+#first rowwidth=4,height=3, font=("Arial",40)
+button1=Button(frame2, text=" ",width=4,height=2, font=("Arial",35), relief=RAISED, bg="orange", borderwidth=5)
 button1.grid(row=0,column=0)
 button1.bind("<Button-1>", play)
 
-button2=Button(frame2,text=" ", width=16, height=8)
+button2=Button(frame2,text=" ",width=4,height=2, font=("Arial",35),relief=RAISED, bg="orange", borderwidth=5)
 button2.grid(row=0,column=1)
+button2.bind("<Button-1>", play)
 
-button3=Button(frame2, text=" ", width=16, height=8)
+
+button3=Button(frame2, text=" ", width=4,height=2, font=("Arial",35),relief=RAISED, bg="orange", borderwidth=5)
 button3.grid(row=0, column=2)
+button3.bind("<Button-1>", play)
 
 #second row
-button1=Button(frame2, text=" ",width=16,height=8)
+button1=Button(frame2, text=" ",width=4,height=2, font=("Arial",35),relief=RAISED, bg="orange", borderwidth=5)
 button1.grid(row=1,column=0)
+button1.bind("<Button-1>", play)
 
-button2=Button(frame2,text=" ", width=16, height=8)
+button2=Button(frame2,text=" ", width=4,height=2, font=("Arial",35),relief=RAISED, bg="orange", borderwidth=5)
 button2.grid(row=1,column=1)
+button2.bind("<Button-1>", play)
 
-button3=Button(frame2, text=" ", width=16, height=8)
+button3=Button(frame2, text=" ", width=4,height=2, font=("Arial",35),relief=RAISED, bg="orange", borderwidth=5)
 button3.grid(row=1, column=2)
+button3.bind("<Button-1>", play)
 
 #third row
-button1=Button(frame2, text=" ",width=16,height=8)
+button1=Button(frame2, text=" ",width=4,height=2, font=("Arial",35),relief=RAISED, bg="orange", borderwidth=5)
 button1.grid(row=2,column=0)
+button1.bind("<Button-1>", play)
 
-button2=Button(frame2,text=" ", width=16, height=8)
+button2=Button(frame2,text=" ", width=4,height=2, font=("Arial",35),relief=RAISED, bg="orange", borderwidth=5)
 button2.grid(row=2,column=1)
+button2.bind("<Button-1>", play)
 
-button3=Button(frame2, text=" ", width=16, height=8)
+button3=Button(frame2, text=" ", width=4,height=2, font=("Arial",35),relief=RAISED, bg="orange", borderwidth=5)
 button3.grid(row=2, column=2)
+button3.bind("<Button-1>", play)
 
 
 
